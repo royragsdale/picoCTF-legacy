addbcc6f0a3fc11433af2919190a2d8f636db2d9
Christopher Ganas <ganaschristopher@gmail.com>
2015-03-26 12:06:03 -0400

    Added security note for user creation.

--- picoCTF-web/api/api/routes/user.py
+++ picoCTF-web/api/api/routes/user.py
@@ -23,6 +23,9 @@ def get_shell_account_hook():
 @blueprint.route('/create', methods=['POST'])
 @api_wrapper
 def create_user_hook():
+    # This is safe even if the user were to additionally add an admin=true to
+    # the data as create_user_request does not call create_user in such a way
+    # that it could possibly create an admin user.
     new_uid = api.user.create_user_request(api.common.flat_multi(request.form))
     session['uid'] = new_uid
     return WebSuccess("User '{}' registered successfully!".format(request.form["username"]))
