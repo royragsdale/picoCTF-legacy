7be5baac71f4af0bda45e8ee871db03d04846c2d
Christopher Ganas <cganas@forallsecure.com>
2015-05-26 11:39:24 -0400

    Added doc to local_update.

--- picoCTF-shell/shell_manager/problem_repo.py
+++ picoCTF-shell/shell_manager/problem_repo.py
@@ -7,7 +7,15 @@ import spur, gzip
 from shutil import copy2
 from os.path import join
 
-def update(repo_path, deb_paths=[]):
+def local_update(repo_path, deb_paths=[]):
+    """
+    Updates a local deb repository by copying debs and running scanpackages.
+
+    Args:
+        repo_path: the path to the local repository.
+        dep_paths: list of problem deb paths to copy.
+    """
+
     [copy2(deb_path, repo_path) for deb_path in deb_paths]
 
     shell = spur.LocalShell()
