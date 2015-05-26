4fdf695846caaf09450118627c1463f19f76a808
Christopher Ganas <cganas@forallsecure.com>
2015-05-26 12:04:10 -0400

    Added publish operations to command line interface.

--- picoCTF-shell/shell_manager/run.py
+++ picoCTF-shell/shell_manager/run.py
@@ -6,26 +6,27 @@ Shell Manager -- Tools for deploying and packaging problems.
 
 from argparse import ArgumentParser
 
-from shell_manager.package import problem_builder
+from shell_manager.package import problem_builder, bundle_problems
 from shell_manager.problem import migrate_problems
+from shell_manager.problem_repo import update_repo
 
 def main():
     parser = ArgumentParser(description="Shell Manager")
     subparsers = parser.add_subparsers(help="package problem for distribution")
 
     problem_package_parser = subparsers.add_parser("package", help="problem package management")
-
-    #build_subparser = problem_packager_parser.add_subparsers(help="build deb package from problem source")
-
-    #problem_package_build_parser = build_subparser.add_parser("build")
-    problem_package_parser.add_argument("-r", "--repository", default="/usr/local/ctf-packages",
-                                              help="Location of problem repository.")
     problem_package_parser.add_argument("-o", "--out", help="folder to store problem package.")
-    problem_package_parser.add_argument("problem_paths", nargs="+", type=str, help="paths to problems.")
+    problem_package_parser.add_argument("problem_paths", nargs="*", type=str, help="paths to problems.")
     problem_package_parser.set_defaults(func=problem_builder)
 
-    migration_parser = subparsers.add_parser("migrate", help="migrate legacy problem formats")
+    publish_parser = subparsers.add_parser("publish", help="publish packaged problems")
+    publish_parser.add_argument("-r", "--repository", default="/usr/local/ctf-packages",
+                                              help="Location of problem repository.")
+    publish_parser.add_argument("repo_type", choices=["local", "remote"])
+    publish_parser.add_argument("package_paths", nargs="+", type=str, help="problem packages to publish.")
+    publish_parser.set_defaults(func=update_repo)
 
+    migration_parser = subparsers.add_parser("migrate", help="migrate legacy problem formats")
     migration_parser.add_argument("-i", "--interactive", action="store_true", help="update problem fields interactively")
     migration_parser.add_argument("-d", "--dry", action="store_true", help="don't make persistent changes.")
     migration_parser.add_argument("-l", "--legacy-format", default="cyberstakes2014", choices=["cyberstakes2014"],
@@ -34,6 +35,12 @@ def main():
                                   default=[], help="field:value used to override new defaults.")
     migration_parser.add_argument("problem_paths", nargs="+", type=str, help="paths to problems.")
     migration_parser.set_defaults(func=migrate_problems)
+
+    bundle_parser = subparsers.add_parser("bundle", help="create a bundle of problems")
+    bundle_parser.add_argument("package_names", nargs="+", type=str, help="problem packages to bundle.")
+    bundle_parser.add_argument("-o", "--out", type=str, help="path to deb to write to.", required=True)
+    bundle_parser.set_defaults(func=bundle_problems)
+
     args = parser.parse_args()
 
     #Call the default function
