ce40e82a9a6f81ba07d9326305b340f71b79286c
Christopher Ganas <cganas@forallsecure.com>
2015-05-28 10:56:35 -0400

    We need to be using python3 for problem api.

--- picoCTF-shell/install.sh
+++ picoCTF-shell/install.sh
@@ -4,7 +4,7 @@
 apt-get install dpkg dpkg-dev fakeroot python3 python3-pip
 
 #Build wheel package
-python2 setup.py bdist_wheel
+python3 setup.py bdist_wheel
 wheel=$(find . | grep "\.whl$")
 
-pip2 install --upgrade $wheel
+pip3 install --upgrade $wheel
