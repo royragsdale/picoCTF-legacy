5c3dcf6b4fed93d7456c108fa9cdd14356a425d6
Tim Becker <tjbecker@cmu.edu>
2015-06-15 10:32:34 -0400

    Updated the install script

--- picoCTF-shell/install.sh
+++ picoCTF-shell/install.sh
@@ -3,8 +3,4 @@
 #Dependencies
 apt-get install dpkg dpkg-dev fakeroot python3 python3-pip
 
-#Build wheel package
-python3 setup.py bdist_wheel
-wheel=$(find . | grep "\.whl$")
-
-pip3 install --upgrade $wheel
+pip3 install --upgrade .
