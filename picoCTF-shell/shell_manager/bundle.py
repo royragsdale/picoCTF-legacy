c4feabef67a06ceeb00818ca8123b3ef65df914f
Christopher Ganas <cganas@forallsecure.com>
2015-06-18 13:58:05 -0400

    Create the staging directories for the bundle.

--- picoCTF-shell/shell_manager/bundle.py
+++ picoCTF-shell/shell_manager/bundle.py
@@ -2,5 +2,22 @@
 Bundling operation for the shell manager. A special case of packaging.
 """
 
+from os import getcwd, makedirs
+from os.path import join, isdir
+
+from shell_manager.problem import get_problem
+from shell_manager.package import get_problem_root, DEB_DEFAULTS
+from shell_manager.package import sanitize_name
+
 def bundle_problems(args, config):
-    pass
+    for package_name in args.package_names:
+        installed_path = get_problem_root(package_name, absolute=True)
+        if not isdir(installed_path) or not get_problem(installed_path):
+            raise Exception("'{}' is not an installed problem.".format(package_name))
+
+    paths = {"working": getcwd() if args.out is None else args.out}
+    paths["staging"] = join(paths["working"], "__staging")
+    paths["debian"] = join(paths["staging"], "DEBIAN")
+
+    [makedirs(staging_path) for _, staging_path in paths.items() if not isdir(staging_path)]
+
