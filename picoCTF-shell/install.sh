18e183b7310a7fa638a6e1b65cf682063df5da01
Tim Becker <tjbecker@cmu.edu>
2015-06-18 12:41:36 -0400

    Add nginx dependency

--- picoCTF-shell/install.sh
+++ picoCTF-shell/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #Dependencies
-apt-get install dpkg dpkg-dev fakeroot python3 python3-pip socat
+apt-get install dpkg dpkg-dev fakeroot python3 python3-pip socat nginx
 
 pip3 install --upgrade .
