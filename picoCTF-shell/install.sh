22885dad883efcc46f7f712d922ab6f46b966603
Tim Becker <tjbecker@cmu.edu>
2015-07-02 15:50:04 -0400

    Use latest version of pip!

--- picoCTF-shell/install.sh
+++ picoCTF-shell/install.sh
@@ -3,4 +3,8 @@
 #Dependencies
 apt-get install -y dpkg dpkg-dev fakeroot python3 python3-pip socat nginx php5-cli gcc-multilib
 
-pip3 install --upgrade .
+pip3 install --upgrade pip
+
+sudo apt uninstall python3-pip
+
+bash -c 'pip3 install --upgrade .'
