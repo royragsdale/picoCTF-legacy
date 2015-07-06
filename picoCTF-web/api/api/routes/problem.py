bc67db045373a538ec7eb21a7d89a3cbbad1b4ac
Tim Becker <tjbecker@cmu.edu>
2015-07-06 11:07:33 -0400

    Fixed bug in displaying of inserted problems message

--- picoCTF-web/api/api/routes/problem.py
+++ picoCTF-web/api/api/routes/problem.py
@@ -107,5 +107,5 @@ def request_problem_hint_hook():
 def load_problems():
     data = json.loads(request.form.get("competition_data", ""))
 
-    num_inserted = api.problem.load_published(data)
-    return WebSuccess("Inserted {} problems.".format(num_inserted))
+    api.problem.load_published(data)
+    return WebSuccess("Inserted {} problems.".format(len(data["problems"])))
