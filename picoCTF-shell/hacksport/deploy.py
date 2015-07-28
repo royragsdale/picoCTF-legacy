b248361e74feec6b4074a6098a32ed4cb74892a6
Tim Becker <tjbecker@cmu.edu>
2015-07-28 18:07:33 -0400

    Lock file will only prevent simultaneous deployment

--- picoCTF-shell/hacksport/deploy.py
+++ picoCTF-shell/hacksport/deploy.py
@@ -53,6 +53,7 @@ import os
 import json
 import shutil
 import functools
+import traceback
 
 def challenge_meta(attributes):
     """
@@ -557,15 +558,19 @@ def deploy_problems(args, config):
         with open(lock_file, "w") as f:
             f.write("1")
 
-    for path in problems:
-        if args.dry and os.path.isdir(path):
-            deploy_problem(path, instances=args.num_instances, test=args.dry,
-                            deployment_directory=args.deployment_directory)
-        elif os.path.isdir(os.path.join(get_problem_root(path, absolute=True))):
-            deploy_problem(os.path.join(get_problem_root(path, absolute=True)), instances=args.num_instances,
-                            test=args.dry, deployment_directory=args.deployment_directory)
-        else:
-            raise Exception("Problem path {} cannot be found".format(path))
+    try:
+        for path in problems:
+            if args.dry and os.path.isdir(path):
+                deploy_problem(path, instances=args.num_instances, test=args.dry,
+                                deployment_directory=args.deployment_directory)
+            elif os.path.isdir(os.path.join(get_problem_root(path, absolute=True))):
+                deploy_problem(os.path.join(get_problem_root(path, absolute=True)), instances=args.num_instances,
+                                test=args.dry, deployment_directory=args.deployment_directory)
+            else:
+                raise Exception("Problem path {} cannot be found".format(path))
 
-    if not args.dry:
-        os.remove(lock_file)
+    except Exception as e:
+        traceback.print_exc()
+    finally:
+        if not args.dry:
+            os.remove(lock_file)
