b2ec7345feb27a341ab3ade51cc94c3d9f8ad636
Tim Becker <tjbecker@cmu.edu>
2015-07-28 20:00:13 -0400

    Better set up of shell server, and we now preserve comfig.py

--- picoCTF-shell/install.sh
+++ picoCTF-shell/install.sh
@@ -3,14 +3,20 @@
 # get directory of this script
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+#if config.py exists, back it up
+mkdir /tmp/hacksports/
+cp /opt/hacksports/config.py /tmp/hacksports/config.py
+
 #Dependencies
 apt-get install -y dpkg dpkg-dev fakeroot python3 python3-pip socat nginx php5-cli gcc-multilib shellinabox
 
 pip3 install --upgrade pip
-
 apt-get remove -y --force-yes python3-pip
 
 bash -c 'pip3 install --upgrade --verbose .'
+
+# restore config.py if backed up
+cp /tmp/hacksports/config.py /opt/hacksports/config.py
 
 # disable apache if it's running
 systemctl disable apache2
@@ -33,3 +39,12 @@ sudo service sshd restart
 easy_install pip
 pip2 install requests
 groupadd competitors
+
+# disable ASLR
+echo "kernel.randomize_va_space=0" >> /etc/sysctl.conf
+sysctl -p
+
+# set hostname
+hostname shell
+echo "shell" > /etc/hostname
+echo -e "127.0.0.1\tshell" > /etc/hosts
