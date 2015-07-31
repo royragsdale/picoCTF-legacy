6f4ff582afffedca3f1dc1263e51fc465227722b
Thanassis Avgerinos <thanassis.avgerinos@gmail.com>
2015-07-31 15:26:32 -0400

    Clean the cache of .deb files after installing.

--- picoCTF-shell/deploy_cyberstakes.sh
+++ picoCTF-shell/deploy_cyberstakes.sh
@@ -6,5 +6,6 @@ INSTANCES=${1:-3}
 sudo shell_manager clean
 
 sudo apt-get install -y --force-yes cyberstakes-online-2014
+sudo apt-get clean
 
 sudo shell_manager deploy -b cyberstakes-online-2014 -n $INSTANCES
