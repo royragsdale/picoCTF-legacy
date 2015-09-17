a20ef1ac50d19d1c9235d8e3757360a4a03ad309
Christopher Ganas <cganas@forallsecure.com>
2015-09-16 23:50:39 -0400

    Added missing mail configuration. Removed autogen routes.

--- picoCTF-web/api/app.py
+++ picoCTF-web/api/app.py
@@ -20,7 +20,6 @@ from api.annotations import api_wrapper, require_login, require_teacher, require
 from api.annotations import block_before_competition, block_after_competition
 from api.annotations import log_action
 
-import api.routes.autogen
 import api.routes.user
 import api.routes.team
 import api.routes.stats
@@ -52,12 +51,14 @@ def config_app(*args, **kwargs):
 
     if settings["email"]["enable_email"]:
         app.config["MAIL_SERVER"] = settings["email"]["smtp_url"]
+        app.config["MAIL_PORT"] = settings["email"]["smtp_port"]
         app.config["MAIL_USERNAME"] = settings["email"]["email_username"]
         app.config["MAIL_PASSWORD"] = settings["email"]["email_password"]
+        app.config["MAIL_DEFAULT_SENDER"] = settings["email"]["from_addr"]
+        app.config["MAIL_USE_TLS"] = True
 
         api.email.mail = Mail(app)
 
-    app.register_blueprint(api.routes.autogen.blueprint, url_prefix="/api/autogen")
     app.register_blueprint(api.routes.user.blueprint, url_prefix="/api/user")
     app.register_blueprint(api.routes.team.blueprint, url_prefix="/api/team")
     app.register_blueprint(api.routes.stats.blueprint, url_prefix="/api/stats")
