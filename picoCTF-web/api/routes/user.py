87665242c320d616e27282904963f1fc0b4040b4
Christopher Ganas <cganas@forallsecure.com>
2015-09-24 13:18:59 -0400

    Add tid to /api/user/status.

--- picoCTF-web/api/routes/user.py
+++ picoCTF-web/api/routes/user.py
@@ -114,6 +114,7 @@ def status_hook():
         "reCAPTCHA_public_key": settings["captcha"]["reCAPTCHA_public_key"],
         "competition_active": api.utilities.check_competition_active(),
         "username": api.user.get_user()['username'] if api.auth.is_logged_in() else "",
+        "tid": api.user.get_user()["tid"] if api.auth.is_logged_in() else "",
         "email_verification": settings["email_verification"]
     }
 
