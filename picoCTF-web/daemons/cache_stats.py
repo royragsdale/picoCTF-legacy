b868c2e413674d4d97b5c2f827ee663c6b4dc846
Christopher Ganas <cganas@forallsecure.com>
2015-11-10 11:41:22 -0500

    Updated cache_stats daemon to explicitly cache versions of score_progression.

--- picoCTF-web/daemons/cache_stats.py
+++ picoCTF-web/daemons/cache_stats.py
@@ -11,15 +11,16 @@ def cache(f, *args, **kwargs):
 
 def run():
     print("Caching the public scoreboard entries...")
-    cache(api.stats.get_all_team_scores)
+    cache(api.stats.get_all_team_scores, eligible=True)
     cache(api.stats.get_all_team_scores, eligible=False)
+
     print("Caching the public scoreboard graph...")
-    cache(api.stats.get_top_teams_score_progressions)
+    cache(api.stats.get_top_teams_score_progressions, eligible=True)
     cache(api.stats.get_top_teams_score_progressions, eligible=False)
 
     print("Caching the scoreboard graph for each group...")
     for group in api.group.get_all_groups():
-        cache(api.stats.get_top_teams_score_progressions, gid=group['gid'])
+        cache(api.stats.get_top_teams_score_progressions, gid=group['gid'], eligible=True)
 
     print("Caching number of solves for each problem.")
     for problem in api.problem.get_all_problems():
