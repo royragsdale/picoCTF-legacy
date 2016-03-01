46b3dac7f17aeaecc09d6f24916bc79945aff1f9
Roy Ragsdale <roy@apexcno.com>
2016-03-01 07:45:46 -0500

    Added deployment two private-problems bundles
    - Removed the installation of the Example Challenges

--- ansible/scripts/deploy_private.sh
+++ ansible/scripts/deploy_private.sh
@@ -15,3 +15,16 @@ do
     apt-get install -fy
 done
 
+
+shell_manager bundle -s $USER_HOME -o $USER_HOME/bundles $EXAMPLE_PROBLEMS_ROOT/Bundles/availible-challenges.json
+shell_manager bundle -s $USER_HOME -o $USER_HOME/bundles $EXAMPLE_PROBLEMS_ROOT/Bundles/storyline-stubs.json
+
+for f in $USER_HOME/bundles/*
+do
+    echo "Installing bundle: $f..."
+    dpkg -i $f
+    apt-get install -fy
+done
+
+shell_manager deploy -n 2 -b availible-challenges
+shell_manager deploy -n 2 -b storyline-stubs
