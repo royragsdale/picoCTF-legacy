cea60d98f753a884b91d2b9bac4d38bc073b386b
Roy Ragsdale <roy@apexcno.com>
2016-03-08 23:40:03 -0500

    Clean slate after picoCTF-Platform-2 "release"

--- api/graders/test.py
+++
@@ -1,4 +0,0 @@
-def grade(arg, key):
-    if key == "test":
-        return True, "You've successfully solved the problem!"
-    return False, "That was an incorrect solution. :("
--- api/run_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-test_path="./tests"
-
-if [[ -n "$1" ]]; then
-  test_path=$1
-fi
-
-python3.4 -b -m pytest --showlocals -s -v "$test_path"
