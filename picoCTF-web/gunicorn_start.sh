ebf4e7b0a9b78c5c66a7be6d94ba967fbe20c043
Roy Ragsdale <roy@apexcno.com>
2016-03-15 16:22:50 -0400

    Cleanup top level web directory
    - ctf.nginx is templated in ansible
    - gunicorn_start.sh and install.sh are represented in ansible

--- picoCTF-web/gunicorn_start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-WORKERS=${1:-4}
-gunicorn -b 0.0.0.0:8000 -w $WORKERS 'api.app:config_app()'
--- picoCTF-web/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#The infrastructure is now just a PIP package.
-pip3 install --upgrade .
