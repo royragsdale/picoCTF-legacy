e342e6621ced0c97efd59b6c4ce8c7092cec6025
Roy Ragsdale <roy@apexcno.com>
2016-03-19 17:56:42 -0400

    Reorganized auto-configuration
    - Also added a start_competition script

--- ansible/scripts/start_competition.py
+++ ansible/scripts/start_competition.py
@@ -1,15 +1,26 @@
+#!/usr/bin/python3
+
+# Simple script to programmaticly start a competition useful for development
+# and testing purposes. Defaults to 1 year.
+
+# If using a custom APP_SETTINGS_FILE, ensure the appropriate
+# environment variable is set prior to running this script. This script is best
+# run from the pico-web role (ansible/roles/pico-web/tasks/main.yml)
+
+# [TODO] consider consolidating into an updated picoCTF-web/api/api_manager.py
+# [TODO] paramaterize
+
 import sys
-
-sys.path.append("/vagrant/ctf-infrastructure/api")
-
-import api
-
 from datetime import datetime, timedelta
 
-settings = api.config.get_settings()
-settings["start_time"] = datetime.now()
-settings["end_time"] = settings["start_time"] + timedelta(weeks=1)
+# The picoCTF API
+import api
 
-api.config.change_settings(settings)
+def main():
+    settings = api.config.get_settings()
+    settings["start_time"] = datetime.now()
+    settings["end_time"] = settings["start_time"] + timedelta(weeks=52)
+    api.config.change_settings(settings)
 
-print("Started the competition")
+if __name__ == "__main__":
+    main()
